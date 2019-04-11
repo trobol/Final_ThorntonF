3455a4aea41f7c81d598a9bdb53bf114f05e7148
thorfernbacher <thorfernbacher@gmail.com>
2019-04-11 10:49:22 -0400

    added training window

--- Assets/NEAT/Scripts/NetworkType.cs
+++ Assets/NEAT/Scripts/NetworkType.cs
@@ -1,25 +1,93 @@
+using UnityEngine;
+using System.Collections;
+using System.IO;
 namespace NEAT
 {
 
-	public class NetworkType {
-		string name;
-		int generations;
+	public class NetworkType
+	{
+		public string name = "Untitled";
+		private static string baseDirectory = Application.dataPath + "/NEAT/Networks/";
+		public bool SetName(string value)
+		{
+			if (value == name) return true;
+			string directoryPath = baseDirectory + value;
+
+
+			if (!Directory.Exists(directoryPath))
+			{
+				name = value;
+				return true;
+			}
+
+
+			return false;
+		}
+
+		public int inputs = 1, outputs = 1;
+		public int generations;
+		public bool trained = false;
 		NetworkList[] generation;
-		public NetworkType(string n, int gens, NetworkList g) {
+		public NetworkType(string n, int gens, NetworkList[] g)
+		{
 			name = n;
 			generations = gens;
 			generation = g;
 		}
-	}
-
-	public static class NetworkLoader {
-		static NetworkType Load(string name) {
-
-
+		public NetworkType() { }
+		public NetworkType(NetworkType n)
+		{
+			name = n.name + " Copy";
+			inputs = n.inputs;
+			outputs = n.outputs;
 		}
-	}
+		public void Save()
+		{
+			string directoryPath = baseDirectory + name;
+			if (!Directory.Exists(directoryPath))
+			{
+				Directory.CreateDirectory(directoryPath);
+			}
 
-	public class NetworkList {
+			string dataAsJson = JsonUtility.ToJson(this);
+
+			File.WriteAllText(directoryPath + "/data.json", dataAsJson);
+		}
+
+
+		public static NetworkType Load(string n)
+		{
+			string filePath = baseDirectory + n + "/data.json";
+
+			if (File.Exists(filePath))
+			{
+				string dataAsJson = File.ReadAllText(filePath);
+				return JsonUtility.FromJson<NetworkType>(dataAsJson);
+			}
+			return null;
+		}
+		public static NetworkType LoadPath(string path)
+		{
+			if (File.Exists(path + "/data.json"))
+			{
+				string dataAsJson = File.ReadAllText(path + "/data.json");
+				return JsonUtility.FromJson<NetworkType>(dataAsJson);
+			}
+
+			return null;
+		}
+		public static void LoadList()
+		{
+			string[] directories = Directory.GetDirectories(baseDirectory);
+			networkTypes = new NetworkType[directories.Length];
+			for (int i = 0; i < directories.Length; i++)
+			{
+
+				networkTypes[i] = LoadPath(directories[i]);
+			}
+		}
+
+		public static NetworkType[] networkTypes;
 
 	}
 }
---
+++ Assets/NEAT/Scripts/TrainingWindow.cs
@@ -0,0 +1,189 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using NEAT;
+
+public class TrainingManagerWindow : EditorWindow
+
+{
+	NetworkType loadedType = null;
+	Network loadedNetwork = null;
+
+	GUIStyle networkTypeListStyle = new GUIStyle();
+	[MenuItem("NEAT/Training")]
+	public static void ShowWindow()
+	{
+		TrainingManagerWindow window = (TrainingManagerWindow)EditorWindow.GetWindow(typeof(TrainingManagerWindow));
+		window.Start();
+	}
+	public void Start()
+	{
+		networkTypeListStyle.alignment = TextAnchor.MiddleLeft;
+
+	}
+
+	void OnGUI()
+	{
+		DrawToolBar();
+		DrawLoadedTypes();
+		if (!DrawNetworkType()) return;
+
+		DrawNetwork();
+	}
+	void DrawLoadedTypes()
+	{
+		if (NetworkType.networkTypes == null) return;
+		GUILayout.BeginVertical(EditorStyles.textArea);
+		if (!viewNetworkTypes) return;
+		foreach (NetworkType nt in NetworkType.networkTypes)
+		{
+			if (nt == null) continue;
+			if (GUILayout.Button(nt.name + "  Inputs:  " + nt.inputs + "  Outputs: ", networkTypeListStyle))
+			{
+				loadedType = NetworkType.Load(nt.name);
+			}
+		}
+		GUILayout.EndVertical();
+
+	}
+	bool DrawNetworkType()
+	{
+		if (loadedType == null)
+		{
+			GUILayout.Label("Open or create a NetworkType to View", EditorStyles.boldLabel);
+			return false;
+		}
+		GUILayout.Label("Network Type", EditorStyles.boldLabel);
+		loadedType.SetName(EditorGUILayout.DelayedTextField("Name", loadedType.name));
+		loadedType.inputs = EditorGUILayout.DelayedIntField("Inputs", loadedType.inputs);
+		loadedType.outputs = EditorGUILayout.DelayedIntField("Outputs", loadedType.outputs);
+
+		return true;
+	}
+	bool viewNetworkTypes = false;
+	bool showNetwork = false;
+	void DrawNetwork()
+	{
+		showNetwork = EditorGUILayout.Foldout(showNetwork, "Network Visual", true);
+		if (!showNetwork) return;
+		if (loadedNetwork == null)
+		{
+			return;
+		}
+		// Begin to draw a horizontal layout, using the helpBox EditorStyle
+		GUILayout.BeginHorizontal(EditorStyles.helpBox);
+
+		// Reserve GUI space with a width from 10 to 10000, and a fixed height of 200, and 
+		// cache it as a rectangle.
+		Rect layoutRectangle = GUILayoutUtility.GetRect(10, 10000, 200, 200);
+		if (Event.current.type == EventType.Repaint)
+		{
+			// If we are currently in the Repaint event, begin to draw a clip of the size of 
+			// previously reserved rectangle, and push the current matrix for drawing.
+			GUI.BeginClip(layoutRectangle);
+			GL.PushMatrix();
+
+			// Clear the current render buffer, setting a new background colour, and set our
+			// material for rendering.
+			GL.Clear(true, false, Color.black);
+
+
+			// Start drawing in OpenGL Quads, to draw the background canvas. Set the
+			// colour black as the current OpenGL drawing colour, and draw a quad covering
+			// the dimensions of the layoutRectangle.
+			GL.Begin(GL.QUADS);
+			GL.Color(Color.black);
+			GL.Vertex3(0, 0, 0);
+			GL.Vertex3(layoutRectangle.width, 0, 0);
+			GL.Vertex3(layoutRectangle.width, layoutRectangle.height, 0);
+			GL.Vertex3(0, layoutRectangle.height, 0);
+			GL.End();
+
+			// Start drawing in OpenGL Lines, to draw the lines of the grid.
+			GL.Begin(GL.LINES);
+
+			// Store measurement values to determine the offset, for scrolling animation,
+			// and the line count, for drawing the grid.
+			int offset = (Time.frameCount * 2) % 50;
+			int count = (int)(layoutRectangle.width / 10) + 20;
+
+			for (int i = 0; i < count; i++)
+			{
+				// For every line being drawn in the grid, create a colour placeholder; if the
+				// current index is divisible by 5, we are at a major segment line; set this
+				// colour to a dark grey. If the current index is not divisible by 5, we are
+				// at a minor segment line; set this colour to a lighter grey. Set the derived
+				// colour as the current OpenGL drawing colour.
+				Color lineColour = (i % 5 == 0 ? new Color(0.5f, 0.5f, 0.5f) : new Color(0.2f, 0.2f, 0.2f));
+				GL.Color(lineColour);
+
+				// Derive a new x co-ordinate from the initial index, converting it straight
+				// into line positions, and move it back to adjust for the animation offset.
+				float x = i * 10 - offset;
+
+				if (x >= 0 && x < layoutRectangle.width)
+				{
+					// If the current derived x position is within the bounds of the
+					// rectangle, draw another vertical line.
+					GL.Vertex3(x, 0, 0);
+					GL.Vertex3(x, layoutRectangle.height, 0);
+				}
+
+				if (i < layoutRectangle.height / 10)
+				{
+					// Convert the current index value into a y position, and if it is within
+					// the bounds of the rectangle, draw another horizontal line.
+					GL.Vertex3(0, i * 10, 0);
+					GL.Vertex3(layoutRectangle.width, i * 10, 0);
+				}
+			}
+
+			// End lines drawing.
+			GL.End();
+
+			// Pop the current matrix for rendering, and end the drawing clip.
+			GL.PopMatrix();
+			GUI.EndClip();
+		}
+
+		// End our horizontal 
+		GUILayout.EndHorizontal();
+	}
+	void DrawToolBar()
+	{
+		GUILayout.BeginHorizontal(EditorStyles.toolbar);
+		if (viewNetworkTypes)
+		{
+			if (GUILayout.Button("Hide", EditorStyles.toolbarButton, GUILayout.Width(150)))
+			{
+				viewNetworkTypes = false;
+			}
+			if (GUILayout.Button("Refresh", EditorStyles.toolbarButton, GUILayout.Width(150)))
+			{
+				NetworkType.LoadList();
+			}
+		}
+		else
+		{
+			if (GUILayout.Button("Open", EditorStyles.toolbarButton, GUILayout.Width(300)))
+			{
+				viewNetworkTypes = true;
+				NetworkType.LoadList();
+			}
+		}
+
+
+		if (GUILayout.Button("New", EditorStyles.toolbarButton))
+		{
+			if (loadedType != null) loadedType.Save();
+			loadedType = new NetworkType();
+		}
+		EditorGUI.BeginDisabledGroup(loadedType == null);
+		if (GUILayout.Button("Duplicate", EditorStyles.toolbarButton))
+		{
+			loadedType = new NetworkType(loadedType);
+		}
+		EditorGUI.EndDisabledGroup();
+		GUILayout.EndHorizontal();
+	}
+}
